af8007e86382578299ca5ee1bf1562ef5619f8ab
PaliJunctureSeparator <162350300+PaliJunctureSeparator@users.noreply.github.com>
2024-04-18 15:55:58 +0700

    Update 0_Pāḷi_Text_Juncture_Separator.py

--- pages/0_Pāḷi_Text_Juncture_Separator.py
+++ pages/0_Pāḷi_Text_Juncture_Separator.py
@@ -24,7 +24,7 @@ st.markdown("<h5 style='text-align: center;'>Read Pāḷi verses fluently.</h5>"
 st.divider()
 """
 **What is it and who is it for?** \n
-    The _Pāḷi Text Juncture Splitter_ is a fragmentizing tool that break up Pāḷi texts based on the class of each syllable (heavy or light) so that you may read them _effortlessly_ with just the right tempo.\n
+The _Pāḷi Text Juncture Splitter_ is a fragmentizing tool that break up Pāḷi texts based on the class of each syllable (heavy or light) so that you may read them _effortlessly_ with just the right tempo.\n
 It is perfect for beginners and intermediates in Pāḷi reading, Pāḷi instructors looking for a teaching aid, as well as those who would like to proficiently chant Parittā verses.\n
 \n
 \n
