85a8fa64ac4b534d3bed5631635ff48e4f4aca93
PaliJunctureSeparator <162350300+PaliJunctureSeparator@users.noreply.github.com>
2024-04-18 17:57:16 +0700

    Update 0_Pāḷi_Text_Juncture_Separator.py

--- pages/0_Pāḷi_Text_Juncture_Separator.py
+++ pages/0_Pāḷi_Text_Juncture_Separator.py
@@ -2739,8 +2739,4 @@ show_app_code = st.sidebar.checkbox (label='Show app code')
 if show_app_code:
     show_code(animation_demo)
 
- st.sidebar.markdown(
-    """
-    View [GitHub repository](https://github.com/PaliJunctureSeparator/Pali-Juncture-Splitter.git)
-    """
-    )
+ st.sidebar.markdown("View [GitHub repository](https://github.com/PaliJunctureSeparator/Pali-Juncture-Splitter.git)")
